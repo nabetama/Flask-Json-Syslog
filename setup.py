66c82d08e4b0a920395176513d02f62d4bbb71a2
nabetama <mao.nabeta@gmail.com>
2015-05-18 00:20:21 +0900

    Set version number to 0.1.2.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Json-Syslog',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/nabetama/Flask-Json-Syslog',
     license='MIT',
     author='Mao Nabeta',
